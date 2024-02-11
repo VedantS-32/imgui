78bc189bdf484fbfbf39f548b9e61634a81b44f3
VedantS <122535961+VedantS-32@users.noreply.github.com>
2024-02-11 17:53:43 +0530

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -21,18 +21,7 @@ project "ImGui"
 		"imstb_textedit.h",
 		"imstb_truetype.h",
 		"imgui_demo.cpp",
-        "backends/imgui_impl_glfw.h",
-        "backends/imgui_impl_glfw.cpp",
-        "backends/imgui_impl_opengl3.h",
-        "backends/imgui_impl_opengl3.cpp",
-        "backends/imgui_impl_opengl3_loader.h"
 	}
-
-    includedirs
-    {
-        "%{IncludeDir.ImGui}",
-		"%{IncludeDir.GLFW}"
-    }
 
 	defines
 	{
